e8655ef85d18f7e815194a316728f8f9f4930b01
Laurin Kerkloh <laurin@kerk-loh.de>
2024-07-22 16:12:50 +0200

    add method to extract testable to a csv

--- lib/llmtest/utils.rb
+++ lib/llmtest/utils.rb
@@ -14,5 +14,28 @@ module Llmtest
         system("rails test #{test_file_path}")
       end
     end
+
+    def self.extract_all_testables
+      csv = CSV.generate do |csv|
+        csv << %w[Model Method Type Callback_Type]
+        Dir.glob(Rails.root.join("app", "models", "*.rb")) do |file|
+          next if File.basename(file) == "application_record.rb"
+          next if File.basename(file) == "current.rb"
+          model_name = File.basename(file, ".rb")
+          model = Llmtest::Model.new(model_name)
+          model.public_method_names.each do |method|
+            csv << [model_name, method, "public", nil]
+          end
+          model.custom_validation_method_names.each do |method|
+            csv << [model_name, method, "validation", nil]
+          end
+          model.custom_callbacks_types_and_method_names.each do |type, method|
+            csv << [model_name, method, "callback", type]
+          end
+        end
+      end
+      puts csv
+      File.write(Rails.root.join("testables.csv"), csv)
+    end
   end
 end
